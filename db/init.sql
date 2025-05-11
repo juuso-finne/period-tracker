33fe2c7a2b977303589c22605e0f0d3c1289ca3c
juuso-finne <juuso.f@gmail.com>
2025-05-11 21:13:08 +0300

    Fix table syntax by replacing double quotes with backticks in init.sql

--- db/init.sql
+++ db/init.sql
@@ -1,21 +1,21 @@
-CREATE TABLE IF NOT EXISTS "user_data" (
-    "id" SERIAL PRIMARY KEY,
-    "username" VARCHAR(255) NOT NULL,
-    "password" VARCHAR(255) NOT NULL
+CREATE TABLE IF NOT EXISTS `user_data` (
+    `id` SERIAL PRIMARY KEY,
+    `username` VARCHAR(255) NOT NULL,
+    `password` VARCHAR(255) NOT NULL
 );
 
-CREATE TABLE IF NOT EXISTS "period_data"(
-    "id" SERIAL PRIMARY KEY,
-    "start" DATE NOT NULL,
-    "end" DATE,
-    "user_id" INT NOT NULL,
-    FOREIGN KEY ("user_id") REFERENCES "user_data"("id") ON DELETE CASCADE
+CREATE TABLE IF NOT EXISTS `period_data`(
+    `id` SERIAL PRIMARY KEY,
+    `start` DATE NOT NULL,
+    `end` DATE,
+    `user_id` INT NOT NULL,
+    FOREIGN KEY (`user_id`) REFERENCES `user_data`(`id`) ON DELETE CASCADE
 );
 
-CREATE TABLE IF NOT EXISTS "default_settings"(
-    "id" SERIAL PRIMARY KEY,
-    "user_id" INT NOT NULL,
-    "plus_minus" INT NOT NULL,
-    "cycle_length" INT NOT NULL,
-    FOREIGN KEY ("user_id") REFERENCES "user_data"("id") ON DELETE CASCADE
+CREATE TABLE IF NOT EXISTS `default_settings`(
+    `id` SERIAL PRIMARY KEY,
+    `user_id` INT NOT NULL,
+    `plus_minus` INT NOT NULL,
+    `cycle_length` INT NOT NULL,
+    FOREIGN KEY (`user_id`) REFERENCES `user_data`(`id`) ON DELETE CASCADE
 );

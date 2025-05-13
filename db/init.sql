c7dcc85ab559feb87bedf2e934d4f8f62b53c96d
juuso-finne <juuso.f@gmail.com>
2025-05-13 13:05:41 +0300

    Changed user id type to UUID

--- db/init.sql
+++ db/init.sql
@@ -1,21 +1,21 @@
-CREATE TABLE IF NOT EXISTS `user_data` (
-    `id` SERIAL PRIMARY KEY,
-    `username` VARCHAR(255) NOT NULL,
-    `password` VARCHAR(255) NOT NULL
+CREATE TABLE IF NOT EXISTS "user_data" (
+    "id" UUID DEFAULT gen_random_uuid() PRIMARY KEY,
+    "username" VARCHAR(255) NOT NULL,
+    "password" TEXT NOT NULL
 );
 
-CREATE TABLE IF NOT EXISTS `period_data`(
-    `id` SERIAL PRIMARY KEY,
-    `start` DATE NOT NULL,
-    `end` DATE,
-    `user_id` INT NOT NULL,
-    FOREIGN KEY (`user_id`) REFERENCES `user_data`(`id`) ON DELETE CASCADE
+CREATE TABLE IF NOT EXISTS "period_data"(
+    "id" SERIAL PRIMARY KEY,
+    "start" DATE NOT NULL,
+    "end" DATE,
+    "user_id" UUID NOT NULL,
+    FOREIGN KEY ("user_id") REFERENCES "user_data"("id") ON DELETE CASCADE ON UPDATE CASCADE
 );
 
-CREATE TABLE IF NOT EXISTS `default_settings`(
-    `id` SERIAL PRIMARY KEY,
-    `user_id` INT NOT NULL,
-    `plus_minus` INT NOT NULL,
-    `cycle_length` INT NOT NULL,
-    FOREIGN KEY (`user_id`) REFERENCES `user_data`(`id`) ON DELETE CASCADE
+CREATE TABLE IF NOT EXISTS "default_settings"(
+    "id" SERIAL PRIMARY KEY,
+    "user_id" UUID NOT NULL,
+    "plus_minus" INT NOT NULL,
+    "cycle_length" INT NOT NULL,
+    FOREIGN KEY ("user_id") REFERENCES "user_data"("id") ON DELETE CASCADE ON UPDATE CASCADE
 );

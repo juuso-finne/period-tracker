096fcff5d885aab83ec008046e06e6b3c66dcc3c
juuso-finne <juuso.f@gmail.com>
2025-05-13 13:10:08 +0300

    Add PeriodData struct to types.go

--- backend/types/types.go
+++ backend/types/types.go
@@ -1,8 +1,7 @@
-package main
+package types
 
 type PeriodData struct{
 	Id int `json:"id"`
 	Start string `json:"start"`
-	End string `json:"end"`
-	UserId int `json:"user_id"`
+	End any `json:"end"`
 }

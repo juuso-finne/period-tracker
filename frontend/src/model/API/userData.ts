31048aca0d7db2fa08b7257139e4acc75ee4dfe8
juuso-finne <juuso.f@gmail.com>
2025-06-10 16:22:39 +0300

    Added API call for login

---
+++ frontend/src/model/API/userData.ts
@@ -0,0 +1,20 @@
+import type { LoginInfo } from "../../types"
+import constructHeaders from "../constructHeaders"
+
+export const login = async (info: LoginInfo): Promise<Response> => {
+    const headers = constructHeaders();
+    const api_url = "http://localhost:5000/users/login"
+
+    const response = await fetch(api_url, {
+        method: "POST",
+        headers: headers,
+        body: JSON.stringify(info)
+    })
+
+    if (!response.ok){
+        const errorText = await response.text();
+        throw new Error(errorText);
+    }
+
+    return response;
+}

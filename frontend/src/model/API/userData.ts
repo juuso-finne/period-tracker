ddd14268efe5bca7713b07cb8f91621745ccdf73
juuso-finne <juuso.f@gmail.com>
2025-06-10 22:08:21 +0300

    Added model for user registration

--- frontend/src/model/API/userData.ts
+++ frontend/src/model/API/userData.ts
@@ -19,3 +19,22 @@ export const login = async (info: LoginInfo): Promise<Response> => {
 
     return response;
 }
+
+export const register = async (info: LoginInfo): Promise<Response> => {
+    const headers = constructHeaders();
+    const api_url = "http://localhost:5000/users/register"
+
+    const response = await fetch(api_url, {
+        method: "POST",
+        headers: headers,
+        credentials: "include",
+        body: JSON.stringify(info)
+    })
+
+    if (!response.ok){
+        const errorText = await response.text();
+        throw new Error(errorText);
+    }
+
+    return response;
+}

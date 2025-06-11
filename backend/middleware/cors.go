0b5c00322afcde40ffb6da475bde1f7a26842829
juuso-finne <juuso.f@gmail.com>
2025-06-11 17:42:09 +0300

    backend: Made token name more consistent in CORS

--- backend/middleware/cors.go
+++ backend/middleware/cors.go
@@ -15,7 +15,7 @@ func CheckCORS(next http.Handler) http.Handler {
     if slices.Contains(originAllowlist, origin) {
       w.Header().Set("Access-Control-Allow-Origin", origin)
 	  w.Header().Set("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE")
-	  w.Header().Set("Access-Control-Allow-Headers", "Content-Type, x-csrf-token")
+	  w.Header().Set("Access-Control-Allow-Headers", "Content-Type, X-CSRF-Token")
 	  w.Header().Set("Access-Control-Allow-Credentials", "true")
     }
     w.Header().Add("Vary", "Origin")

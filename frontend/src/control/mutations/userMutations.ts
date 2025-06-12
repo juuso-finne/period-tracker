9ba6286dd69b2dff10f7a2b020e3a8d451b55835
juuso-finne <juuso.f@gmail.com>
2025-06-12 22:06:23 +0300

    frontend: made login mutation more generic

--- frontend/src/control/mutations/userMutations.ts
+++ frontend/src/control/mutations/userMutations.ts
@@ -3,16 +3,15 @@ import { useMutation } from "@tanstack/react-query"
 import {login} from "../../model/API/userData"
 
 
-export const useLoginMutation = (updateFunction: () => void) => {
+export const useLoginMutation = (loginSuccess: () => void, loginFail: (error: Error) => void) => {
 
     return useMutation({
         mutationFn: login,
         onSuccess: () => {
-            console.log("Login successful")
-            updateFunction();
+            loginSuccess();
         },
         onError: (error) => {
-            console.log(error.message);
+            loginFail(error);
         }
     });
 }

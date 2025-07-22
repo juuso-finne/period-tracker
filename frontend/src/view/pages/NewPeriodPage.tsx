0b29949e67039e30704f55b294d8145467d1b952
juuso-finne <juuso.f@gmail.com>
2025-07-22 17:08:24 +0300

    frontend: Added calendar and notes fields to NewPeriodPage

--- frontend/src/view/pages/NewPeriodPage.tsx
+++ frontend/src/view/pages/NewPeriodPage.tsx
@@ -1,8 +1,9 @@
 import { useQuery } from "@tanstack/react-query"
 import { getPeriodData } from "../../model/API/periodData"
-import { useNavigate } from "react-router-dom";
-import { useEffect, useState } from "react";
+import { useNavigate, useSearchParams } from "react-router-dom";
+import { useEffect, useMemo, useState } from "react";
 import { AuthError, CustomDate } from "../../model/types";
+import Calendar from "../components/scripts/Calendar";
 
 export default function NewPeriodPage() {
     const navigate = useNavigate();
@@ -11,13 +12,25 @@ export default function NewPeriodPage() {
         queryFn: getPeriodData
     });
 
+    const [searchParams, _] = useSearchParams();
+
     const [selectionStart, setSelectionStart] = useState<CustomDate|null>(null);
     const [selectionEnd, setSelectionEnd] = useState<CustomDate|null>(null);
-    const [currentPeriod, setCurrentPeriod] = useState<boolean>(true);
+    const [currentPeriod, setCurrentPeriod] = useState<boolean>(searchParams.get("current") === "true");
+    const [errorText, setErrorText] = useState<string>("");
+    const [notes, setNotes] = useState<string>("");
+
+    const memoizedFixedEnd = useMemo(
+    () => (currentPeriod ? CustomDate.todayAsUTC() : null),
+    [currentPeriod]);
+
+    useEffect(()=>{
+        console.log(notes);
+    },[notes])
 
     useEffect(() => {
     if (error && error instanceof AuthError){
-        navigate("/redirect")
+        navigate("/redirect");
     }
     }, [error, navigate]);
 
@@ -28,14 +41,21 @@ export default function NewPeriodPage() {
     return (
     <>
         {error ? <p>{error.message}</p> : <></>}
-        {(data || []).map((period) =>
-        <div key = {period.id} className="border-2 my-4 p-2 w-fit">
-          <p>id: {period.id}</p>
-          <p>start: {period.start.isoStringDateOnly()}</p>
-          <p>end: {period.end?.isoStringDateOnly()}</p>
-          <p>notes: {period.notes}</p>
-        </div>
-      )}
+        <div>Starting date: {selectionStart?.toLocaleDateString(undefined, {timeZone:"UTC"})}</div>
+        <div>End date: {selectionEnd?.toLocaleDateString(undefined, {timeZone:"UTC"})}</div>
+        <div>Notes:</div>
+        <textarea onChange={e => setNotes(e.target.value)}/>
+        <div>I'm currently on this period</div>
+        <input type="checkbox" onChange={e => setCurrentPeriod(e.target.checked)} defaultChecked={currentPeriod}/>
+            <Calendar
+            mode = "RANGE"
+            periodData={data || []}
+            selectionStart={selectionStart}
+            setSelectionStart={setSelectionStart}
+            selectionEnd={selectionEnd}
+            setSelectionEnd={setSelectionEnd}
+            fixedEnd={memoizedFixedEnd}
+            />
     </>
     )
 }

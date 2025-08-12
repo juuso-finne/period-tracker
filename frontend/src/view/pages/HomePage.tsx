423e99a9c1593d0494645d011f02c58c272d1924
juuso-finne <juuso.f@gmail.com>
2025-08-12 18:00:01 +0300

    frontend: cleaned up front page

--- frontend/src/view/pages/HomePage.tsx
+++ frontend/src/view/pages/HomePage.tsx
@@ -1,38 +1,14 @@
-import { useState, useEffect, useMemo } from "react";
+import { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 import { skipToken, useQueries } from "@tanstack/react-query"
-import Calendar from "../components/scripts/Calendar";
 import { getPeriodData } from "../../model/API/periodData"
 import { getSettingsData } from "../../model/API/settingsData";
 import { getCookie } from "../../control/cookies";
-import { CustomDate, AuthError, type PeriodData, type SettingsData } from "../../model/types";
+import { AuthError, type PeriodData, type SettingsData } from "../../model/types";
 import * as stats from "../../control/periodStats"
 
 function HomePage() {
   const [loggedIn, setLoggedIn] = useState<boolean>(false);
-
-  const [selectionStart, setSelectionStart] = useState<CustomDate|null>(new CustomDate(Date.UTC(2025, 6, 1)));
-  const [selectionEnd, setSelectionEnd] = useState<CustomDate|null>(CustomDate.todayAsUTC());
-  const [singleSelection, setSingleselection ] = useState<{day: CustomDate, period: number | null}|null>(null);
-  const [currentPeriod, setCurrentPeriod] = useState<boolean>(false)
-
-  useEffect(()=>{
-    console.log(singleSelection)
-  },[singleSelection])
-
-  useEffect(()=>{
-    if(currentPeriod){
-      setSelectionEnd(CustomDate.todayAsUTC());
-    }
-  },[currentPeriod])
-
-  useEffect(()=>{
-    if (!selectionEnd || !selectionStart)
-      return
-    console.log(`${selectionStart?.isoStringDateOnly()} - ${selectionEnd?.isoStringDateOnly()}`)
-
-    console.log(CustomDate.todayAsUTC().isBetween(selectionStart, selectionEnd))
-  },[selectionEnd, selectionStart])
 
   const results = useQueries({
     queries: [
@@ -62,57 +38,24 @@ function HomePage() {
     }
   }, [error]);
 
-  const memoizedFixedEnd = useMemo(
-  () => (currentPeriod ? CustomDate.todayAsUTC() : null),
-  [currentPeriod]
-);
-
   if (!loggedIn){
     return(<div>
       <p>Please <Link to={"login"}>log in or register</Link></p>
     </div>)
   }
 
+  if(isFetching){
+    return(<p>Loading...</p>)
+  }
+
 
 
   return (
-    <>
-      <h1 className="text-red-400">Period tracker</h1>
-      {isFetching ? <p>Loading...</p> : <></>}
-      {error ? <p>{error.message}</p> : <></>}
-      {(data || []).map((period) =>
-        <div key = {period.id} className="border-2 my-4 p-2 w-fit">
-          <p>id: {period.id}</p>
-          <p>start: {period.start.isoStringDateOnly()}</p>
-          <p>end: {period.end?.isoStringDateOnly()}</p>
-          <p>notes: {period.notes}</p>
-        </div>
-      )}
+    <div className="flex flex-col gap-4 items-center text-center">
+      <h1 className="text-red-400 text-2xl">Period tracker</h1>
       <div>Welcome, {getCookie("username")}!</div>
-      <div>I'm currently on this period</div>
-      <input type="checkbox" onChange={e => setCurrentPeriod(e.target.checked)} defaultChecked={currentPeriod}/>
-      <div>Selection start: {selectionStart?.isoStringDateOnly()}</div>
-      <div>Selection end: {selectionEnd?.isoStringDateOnly()}</div>
-
       <Prediction data={data} settings={settings}/>
-
-      <Calendar
-        mode = "RANGE"
-        periodData={data || []}
-        selectionStart={selectionStart}
-        setSelectionStart={setSelectionStart}
-        selectionEnd={selectionEnd}
-        setSelectionEnd={setSelectionEnd}
-        fixedEnd={memoizedFixedEnd}
-      />
-
-      <Calendar
-        mode="SINGLE"
-        periodData={data || []}
-        setValue={setSingleselection}
-        value={singleSelection}
-        />
-    </>
+    </div>
   )
 }
 

ed39547d5ef7094e39d33ac71f184fcee344ad82
juuso-finne <juuso.f@gmail.com>
2025-08-12 19:01:33 +0300

    frontend: made PeriodStartButton functional

--- frontend/src/view/components/scripts/PeriodStartButton.tsx
+++ frontend/src/view/components/scripts/PeriodStartButton.tsx
@@ -1,8 +1,31 @@
 import BaseDialog from "./Dialog/BaseDialog"
+import SuccessDialog from "./Dialog/SuccessDialog";
 import { useState } from "react"
+import { useNavigate } from "react-router-dom";
+import { usePostPeriodMutation } from "../../../control/mutations/periodDataMutations";
+import { AuthError, CustomDate } from "../../../model/types";
 
-export default function PeriodStartButton({currentPeriod}:{currentPeriod:boolean}) {
+export default function PeriodStartButton({currentPeriod, setErrorText}:{currentPeriod:boolean, setErrorText: React.Dispatch<React.SetStateAction<string>>}) {
+
     const [isOpen, setIsOpen] = useState(false);
+    const [successOpen, setSuccessOpen] = useState<boolean>(false);
+
+    const navigate = useNavigate();
+
+    const submissionSuccess = () => {
+        setSuccessOpen(true);
+    }
+
+    const submissionFail = (error: Error) =>{
+        if (error instanceof AuthError){
+            navigate("/redirect");
+        }else{
+            setErrorText(error.message);
+        }
+    }
+
+    const mutation = usePostPeriodMutation(submissionSuccess, submissionFail);
+
     if (currentPeriod){
         return(<></>)
     }
@@ -17,10 +40,16 @@ export default function PeriodStartButton({currentPeriod}:{currentPeriod:boolean
             setIsOpen={setIsOpen}
         >
             <div className="flex gap-2 p-4">
-                <button className="btn-primary">
+                <button
+                    className="btn-primary"
+                    onClick={() => mutation.mutate({id:null, start: CustomDate.todayAsUTC(), end: null, notes: ""})}
+                >
                     My period started today
                 </button>
-                <button className="btn-primary">
+                <button
+                    className="btn-primary"
+                    onClick={() => navigate("/new?current=true")}
+                >
                     My period started earlier
                 </button>
                 <button className="btn-primary" onClick={() => setIsOpen(false)}>
@@ -28,6 +57,12 @@ export default function PeriodStartButton({currentPeriod}:{currentPeriod:boolean
                 </button>
             </div>
         </BaseDialog>
+
+        <SuccessDialog
+            message="Period data saved successfully"
+            isOpen={successOpen}
+            setIsOpen={setSuccessOpen}
+        />
     </>
     )
 }

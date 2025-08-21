0193aa5bf4307b88038768885934f23fdbc23fd4
juuso-finne <juuso.f@gmail.com>
2025-08-21 17:01:26 +0300

    backend: allowed origin derived from env variable instead of hard-coding

--- backend/middleware/cors.go
+++ backend/middleware/cors.go
@@ -3,13 +3,25 @@ package middleware
 import (
   "net/http"
   "slices"
+  "os"
+  "fmt"
 )
 
-var originAllowlist = []string{
-  "http://localhost:5173",
-}
+var originAllowlist = []string{}
 
 func CheckCORS(next http.Handler) http.Handler {
+  frontend_url, exists := os.LookupEnv("FROTNEND_URL")
+	if !exists{
+		frontend_url = "http://localhost"
+	}
+
+  frontend_port, exists := os.LookupEnv("FRONTEND_PORT")
+	if !exists{
+		frontend_port = "5173"
+	}
+
+  originAllowlist = append(originAllowlist, fmt.Sprintf("%s:%s", frontend_url, frontend_port))
+
   return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
     origin := r.Header.Get("Origin")
     if r.URL.Path == "/images/" || slices.Contains(originAllowlist, origin) {

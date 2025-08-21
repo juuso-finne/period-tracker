06b6bc6a743520dda2ac3b41984cb7e7e1ec6351
juuso-finne <juuso.f@gmail.com>
2025-08-21 21:07:53 +0300

    frontend: API url is based on .env file insted of hard-coding

--- frontend/src/model/API/_apiRequest_.ts
+++ frontend/src/model/API/_apiRequest_.ts
@@ -4,7 +4,7 @@ type Method = "GET" | "POST" | "PUT" | "DELETE"
 
     const apiRequest = async (method: Method, routeURL: string, body?: string): Promise<Response> => {
     const headers = constructHeaders();
-    const baseUrl = "http://localhost:5000"
+    const baseUrl = getBaseUrl();
     const apiUrl = `${baseUrl}${routeURL}`
 
     const response = await fetch(apiUrl, {
@@ -23,6 +23,14 @@ type Method = "GET" | "POST" | "PUT" | "DELETE"
     }
 
     return response;
+}
+
+
+function getBaseUrl(): string {
+    const server = import.meta.env.VITE_SERVER
+    const port = import.meta.env.VITE_BACKEND_PORT
+    const baseUrl = `${server}${port}`
+    return baseUrl
 }
 
 export default apiRequest;
